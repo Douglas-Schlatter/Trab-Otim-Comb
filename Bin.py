2cc34b1c083f9b5d2b89847215fd55ba65b0bc1c
Douglas-Schlatter <douglas.ardenghi18@gmail.com>
2024-12-03 23:12:29 -0300

    Added bin class to metaheuristic

---
+++ Bin.py
@@ -0,0 +1,5 @@
+class Bin:
+  def __init__(self, id, lowerLimit, upperLimit):
+    self.id = id
+    self.lowerLimit = lowerLimit
+    self.upperLimit = upperLimit
--- ballsAndBins.py
+++ ballsAndBins.py
@@ -1,3 +1,4 @@
+import Bin
 '''A primeira linha do arquivo informa o n´umero de
 recipientes (n). A segunda linha do arquivo informa o n´umero de bolas
 m. Cada linha a partir da terceira linha do arquivo at´e a linha n + 2
@@ -9,12 +10,19 @@ e o limite superior do recipiente i, respectivamente.'''
 file = open('./inf05010_2024-2_B_TP_instances_bins-and-balls/01.txt', 'r')
 lines = file.readlines()
     # Read each line in the file
+
 for index in range(0,len(lines)):
+    #print(index)
+    #print (lines[index])
     if(index == 0):
         numBins = lines[index]
     elif(index == 1):
         numBalls = lines[index]
     else:
+        targetLine=lines[index].split(" ")
+        targetBin = Bin.Bin(index,targetLine[0],targetLine[1])
+        print(targetBin.id, " ",targetBin.lowerLimit)
+        
         #Todo colocar aqui o vetor que guarda o lower e upper de cada bin
-        break
-    print (lines[index])
+        
+    
